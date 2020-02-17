06deae12c3bcc589fef7b029ded59797f31700a1
MarzanShuvo <marzanalam3@gmail.com>
2020-02-17 06:01:58 +0600

    svd

---
+++ matlab/CH01_SEC06_2_3_4.m
@@ -0,0 +1,60 @@
+clear all, close all, clc
+
+load ('/media/marzan/data storage/singular_value_decomposition/DATA/DATA/allFaces.mat')
+
+
+% We use the first 36 people for training data
+trainingFaces = faces(:,1:sum(nfaces(1:36)));
+avgFace = mean(trainingFaces,2);  % size n*m by 1;
+
+% Compute eigenfaces on mean-subtracted training data
+X = trainingFaces-avgFace*ones(1,size(trainingFaces,2));
+[U,S,V] = svd(X,'econ');
+
+imagesc(reshape(avgFace,n,m)) % Plot avg face
+imagesc(reshape(U(:,1),n,m))  % Plot first eigenface
+
+%% Now show eigenface reconstruction of image that was omitted from test set
+
+testFace = faces(:,1+sum(nfaces(1:36))); % First face of person 37
+subplot(2,4,1)
+imagesc(reshape(testFace,n,m))
+count = 1;
+
+
+testFaceMS = testFace - avgFace;
+for r=[25 50 100 200 400 800 1600]
+    count = count+1;
+    subplot(2,4,count)
+    reconFace = avgFace + (U(:,1:r)*(U(:,1:r)'*testFaceMS));
+    imagesc(reshape(reconFace,n,m))
+    title(['r', num2str(r,'%d')])
+end
+
+
+%% Project person 2 and 7 onto PC5 and PC6
+
+P1num = 2;  % Person number 2
+P2num = 7;  % Person number 7
+
+P1 = faces(:,1+sum(nfaces(1:P1num-1)):sum(nfaces(1:P1num)));
+P2 = faces(:,1+sum(nfaces(1:P2num-1)):sum(nfaces(1:P2num)));
+
+P1 = P1 - avgFace*ones(1,size(P1,2));
+P2 = P2 - avgFace*ones(1,size(P2,2));
+
+figure
+subplot(1,2,1), imagesc(reshape(P1(:,1),n,m)); colormap(gray), axis off
+subplot(1,2,2), imagesc(reshape(P2(:,1),n,m)); colormap(gray), axis off
+%%
+
+PCAmodes = [5 6];    % Project onto PCA modes 5 and 6
+PCACoordsP1 = U(:,PCAmodes)'*P1;
+PCACoordsP2 = U(:,PCAmodes)'*P2;
+
+figure
+plot(PCACoordsP1(1,:),PCACoordsP1(2,:),'kd','MarkerFaceColor','k')
+axis([-4000 4000 -4000 4000]), hold on, grid on
+plot(PCACoordsP2(1,:),PCACoordsP2(2,:),'r^', 'MarkerFaceColor','r')
+set(gca,'XTick',[0],'YTick',[0]);
+legend('Person1','Person 2')
